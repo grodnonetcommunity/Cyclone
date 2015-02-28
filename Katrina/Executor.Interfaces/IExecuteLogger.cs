1d381f51ca0aa44e23087f5e64f78e4612a7edbd
Alexander Puzynia <Alexander_Puzynia@epam.com>
2015-02-28 13:40:01 +0300

    [+] Add [IExecutorLogger] interface and fake implementation.

---
+++ Katrina/Executor.Interfaces/IExecuteLogger.cs
@@ -0,0 +1,7 @@
+﻿namespace AV.Cyclone.Katrina.Executor.Interfaces
+{
+    public interface IExecuteLogger
+    {
+        T LogAssign<T>(string expression, string fileNme, int lineNumber, T value);
+    }
+}
---
+++ Katrina/Katrina.Executor/Context.cs
@@ -0,0 +1,9 @@
+﻿using AV.Cyclone.Katrina.Executor.Interfaces;
+
+namespace AV.Cyclone.Katrina.Executor
+{
+    public static class Context
+    {
+        public static IExecuteLogger FakeExecuteLogger { get; set; }
+    }
+}
