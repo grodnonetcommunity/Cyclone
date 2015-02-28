024bcaf98ea8bc8b0a64b01d651f80ff941257c8
Alexander Puzynia <Alexander_Puzynia@epam.com>
2015-02-28 19:25:21 +0300

    [+] Add [TestSolution].

---
+++ .TestSolution/Algorithms/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿namespace TestSolution
+{
+    public static class BinarySearch
+    {
+        public static int Search(int key, int[] array)
+        {
+            var low = 0;
+            var high = array.Length - 1;
+
+            while (low <= high)
+            {
+                var mid = low + (high - low)/2;
+                var value = array[mid];
+
+                if (key > value)
+                {
+                    low = mid + 1;
+                }
+                else if (key < value)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ .TestSolution/Test.Algorithms/BinarySerchTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestSolution;
+
+namespace Test.Algorithms
+{
+    public class BinarySerchTest
+    {
+        [Test]
+        public void LessOrEqualRequired()
+        {
+            var key = 4;
+            var array = new[] {1, 2, 3, 4, 5, 6};
+
+            var index = BinarySearch.Search(key, array);
+
+            Assert.AreEqual(3, index);
+        }
+    }
+}
