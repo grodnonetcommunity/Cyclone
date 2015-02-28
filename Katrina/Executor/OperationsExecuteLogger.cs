913cad521c3e4f3e9be3c7cc00baa578c87220b0
Alexander Puzynia <Alexander_Puzynia@epam.com>
2015-02-28 22:30:22 +0300

    [+] Add and implement operation logger.

---
+++ Katrina/Executor/OperationsExecuteLogger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using AV.Cyclone.Katrina.Executor.Interfaces;
+using AV.Cyclone.Sandy.Models.Operations;
+
+namespace AV.Cyclone.Katrina.Executor
+{
+    public class OperationsExecuteLogger : BaseExecuteLogger
+    {
+        private readonly Stack<List<Operation>> executeStack = new Stack<List<Operation>>();
+        private List<Operation> currentOperations;
+
+        public Dictionary<MethodReference, List<List<Operation>>> MethodCalls { get; } = new Dictionary<MethodReference, List<List<Operation>>>();
+
+        public override void BeginMethod(string methodName, string fileName, int lineNumber)
+        {
+            executeStack.Push(currentOperations);
+            currentOperations = new List<Operation>();
+        }
+
+        public override void EndMethod(string methodName, string fileName, int lineNumber)
+        {
+            var methodOperations = GetMethodOperations(methodName, fileName);
+            methodOperations.Add(currentOperations);
+            currentOperations = executeStack.Pop();
+        }
+
+        public override T LogAssign<T>(string expression, string fileNme, int lineNumber, T value)
+        {
+            var assignOperation = new AssignOperation
+            {
+                FileName = fileNme,
+                LineNumber = lineNumber,
+                VariableName = expression,
+                VariableValue = value
+            };
+            currentOperations.Add(assignOperation);
+            return base.LogAssign(expression, fileNme, lineNumber, value);
+        }
+
+        private List<List<Operation>> GetMethodOperations(string methodName, string fileName)
+        {
+            var methodReference = new MethodReference(fileName, methodName);
+            List<List<Operation>> list;
+            if (MethodCalls.TryGetValue(methodReference, out list))
+                return list;
+            list = new List<List<Operation>>();
+            MethodCalls.Add(methodReference, list);
+            return list;
+        }
+    }
+}
---
+++ Katrina/Test.Executor/OperationsExecuteLoggerTest.cs
@@ -0,0 +1,102 @@
+﻿using System.Linq;
+using AV.Cyclone.Katrina.Executor;
+using NUnit.Framework;
+
+namespace Test.Executor
+{
+    public class OperationsExecuteLoggerTest
+    {
+        [Test]
+        public void MethodCallTest()
+        {
+            var opertaionsExecuteLogger = new OperationsExecuteLogger();
+            opertaionsExecuteLogger.BeginMethod("M", "", 0);
+            opertaionsExecuteLogger.LogAssign("i", "", 1, 0);
+            opertaionsExecuteLogger.EndMethod("M", "", 0);
+
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Count); // Only one method was called
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.First().Count); // And was called only once
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.First()[0].Count); // with one operation
+        }
+
+        [Test]
+        public void InnerCallTest()
+        {
+            var opertaionsExecuteLogger = new OperationsExecuteLogger();
+            opertaionsExecuteLogger.BeginMethod("M1", "", 0);
+            {
+                opertaionsExecuteLogger.LogAssign("i", "", 1, 1);
+                opertaionsExecuteLogger.BeginMethod("M2", "", 2);
+                {
+                    opertaionsExecuteLogger.LogAssign("j", "", 1, 1);
+                }
+                opertaionsExecuteLogger.EndMethod("M2", "", 0);
+            }
+            opertaionsExecuteLogger.EndMethod("M1", "", 0);
+
+            Assert.AreEqual(2, opertaionsExecuteLogger.MethodCalls.Count); // Two methods was called
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.First().Count); // And each called only once
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.First()[0].Count); // with one operation each
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Last().Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Last()[0].Count);
+        }
+
+        [Test]
+        public void InnerCallTest2()
+        {
+            var opertaionsExecuteLogger = new OperationsExecuteLogger();
+            opertaionsExecuteLogger.BeginMethod("M1", "", 0);
+            {
+                opertaionsExecuteLogger.LogAssign("i", "", 1, 1);
+                opertaionsExecuteLogger.BeginMethod("M2", "", 2);
+                {
+                    opertaionsExecuteLogger.LogAssign("j", "", 1, 1);
+                    opertaionsExecuteLogger.BeginMethod("M3", "", 2);
+                    {
+                        opertaionsExecuteLogger.LogAssign("k", "", 1, 1);
+                    }
+                    opertaionsExecuteLogger.EndMethod("M3", "", 0);
+                }
+                opertaionsExecuteLogger.EndMethod("M2", "", 0);
+            }
+            opertaionsExecuteLogger.EndMethod("M1", "", 0);
+
+            Assert.AreEqual(3, opertaionsExecuteLogger.MethodCalls.Count); // Three methods was called
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(0).First().Count); // And each called only once
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(0).First()[0].Count); // with one operation each
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(1).First().Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(1).First()[0].Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(2).First().Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(2).First()[0].Count);
+        }
+
+        [Test]
+        public void SeparateInnerCallTest2()
+        {
+            var opertaionsExecuteLogger = new OperationsExecuteLogger();
+            opertaionsExecuteLogger.BeginMethod("M1", "", 0);
+            {
+                opertaionsExecuteLogger.LogAssign("i", "", 1, 1);
+                opertaionsExecuteLogger.BeginMethod("M2", "", 2);
+                {
+                    opertaionsExecuteLogger.LogAssign("j", "", 1, 1);
+                }
+                opertaionsExecuteLogger.EndMethod("M2", "", 0);
+                opertaionsExecuteLogger.BeginMethod("M3", "", 2);
+                {
+                    opertaionsExecuteLogger.LogAssign("k", "", 1, 1);
+                }
+                opertaionsExecuteLogger.EndMethod("M3", "", 0);
+            }
+            opertaionsExecuteLogger.EndMethod("M1", "", 0);
+
+            Assert.AreEqual(3, opertaionsExecuteLogger.MethodCalls.Count); // Three methods was called
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(0).First().Count); // And each called only once
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(0).First()[0].Count); // with one operation each
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(1).First().Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(1).First()[0].Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(2).First().Count);
+            Assert.AreEqual(1, opertaionsExecuteLogger.MethodCalls.Values.Skip(2).First()[0].Count);
+        }
+    }
+}
