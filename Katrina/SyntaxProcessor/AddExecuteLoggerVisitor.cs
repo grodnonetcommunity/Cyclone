64d9a8aef5810b091a2c72b87dd95be0612f2f0c
Alexander Puzynia <Alexander_Puzynia@epam.com>
2015-02-28 14:58:43 +0300

    [#] Refactor [AddExecuteLoggerVisitor].

--- Katrina/SyntaxProcessor/AddExecuteLoggerVisitor.cs
+++ Katrina/SyntaxProcessor/AddExecuteLoggerVisitor.cs
@@ -13,20 +13,34 @@ namespace AV.Cyclone.Katrina.SyntaxProcessor
             var fileName = node.Identifier.SyntaxTree.FilePath;
             var lineNumber = node.Identifier.SyntaxTree.GetLineSpan(node.Identifier.Span).StartLinePosition.Line;
 
-            var valueExpression = node.Initializer.Value;
+            var invocation = CreateLogAssignInvocationExpression(variableName, fileName, lineNumber, node.Initializer.Value);
+
+            return node.Update(node.Identifier, node.ArgumentList, node.Initializer.Update(node.Initializer.EqualsToken, invocation));
+        }
+
+        private static InvocationExpressionSyntax CreateLogAssignInvocationExpression(string variableName, string fileName,
+            int lineNumber, ExpressionSyntax valueExpression)
+        {
             var memberAccess = CreateMemberAccess("AV.Cyclone.Katrina.Executor.Context.ExecuteLogger.LogAssign");
-            var arguments = SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new []
+            var arguments = SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new[]
             {
-                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression,
-                    SyntaxFactory.Literal(variableName))),
-                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression,
-                    SyntaxFactory.Literal(fileName))),
-                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
-                    SyntaxFactory.Literal(lineNumber))),
+                SyntaxFactory.Argument(CreaetLiteral(variableName)),
+                SyntaxFactory.Argument(CreaetLiteral(fileName)),
+                SyntaxFactory.Argument(CreateLiteral(lineNumber)),
                 SyntaxFactory.Argument(valueExpression)
             }));
-            var invocation = SyntaxFactory.InvocationExpression(memberAccess, arguments);
-            return node.Update(node.Identifier, node.ArgumentList, node.Initializer.Update(node.Initializer.EqualsToken, invocation));
+            return SyntaxFactory.InvocationExpression(memberAccess, arguments);
+        }
+
+        private static LiteralExpressionSyntax CreaetLiteral(string value)
+        {
+            return SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(value));
+        }
+
+        private static LiteralExpressionSyntax CreateLiteral(int lineNumber)
+        {
+            return SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
+                SyntaxFactory.Literal(lineNumber));
         }
 
         public static ExpressionSyntax CreateMemberAccess(string path)
