14cf768af1a2b6e6591e814c74d422f7bd9a8573
max.v <maxvoitas@gmail.com>
2015-02-28 12:52:58 +0300

    Add empty VS Plugin - Cyclone!

---
+++ Cyclone/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+
+using System;
+
+namespace AV.Cyclone
+{
+    static class GuidList
+    {
+        public const string guidCyclonePkgString = "90692408-765a-405e-927a-a8eaebe81bbd";
+        public const string guidCycloneCmdSetString = "446a9d89-6688-41bc-b98d-754008cf9883";
+
+        public static readonly Guid guidCycloneCmdSet = new Guid(guidCycloneCmdSetString);
+    };
+}
