15851846d77d9ecebbb078aed2cc1ff4075c5f35
Alexander Puzynia <Alexander_Puzynia@epam.com>
2015-02-28 14:15:25 +0300

    [+] Add [AddExecuteLoggerVisitor] and test for it.

---
+++ Katarina/Test.SyntaxProcessor/AddExecuteLoggerVisitorTest.cs
@@ -0,0 +1,29 @@
+﻿using AV.Cyclone.Katrina.SyntaxProcessor;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace Test.SyntaxProcessor
+{
+    public class AddExecuteLoggerVisitorTest
+    {
+        [Test]
+        public void SimpleMemberAccessorTest()
+        {
+            var source = "var a = 0";
+            var fullSource = "class C { void M() { " + source + "; } }";
+            var syntaxTree = CSharpSyntaxTree.ParseText(fullSource);
+            var compilationUnit = (CompilationUnitSyntax)syntaxTree.GetRoot();
+            var typeDeclaration = (TypeDeclarationSyntax)compilationUnit.Members[0];
+            var methodDeclaration = (MethodDeclarationSyntax)typeDeclaration.Members[0];
+            var methodBody = methodDeclaration.Body;
+            var firstStatement = methodBody.Statements[0];
+
+            var visitor = new AddExecuteLoggerVisitor();
+            var newFirstStatement = visitor.Visit(firstStatement);
+
+            Assert.AreEqual(@"var a = AV.Cyclone.Katrina.Executor.Context.ExecuteLogger.LogAssign(""a"","""",0,0)", newFirstStatement.ToString());
+        }
+    }
+}
---
+++ Katrina/SyntaxProcessor/AddExecuteLoggerVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AV.Cyclone.Katrina.SyntaxProcessor
+{
+    public class AddExecuteLoggerVisitor : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitVariableDeclarator(VariableDeclaratorSyntax node)
+        {
+            var identifier = node.Identifier;
+            return base.VisitVariableDeclarator(node);
+        }
+    }
+}
