5e76bc403605d40ca0d349538e6507e4140bc575
Alexander Puzynia <Alexander_Puzynia@epam.com>
2015-02-28 15:07:35 +0300

    [#] Refactor [AddExecuteLoggerVisitorTest].

--- Katarina/Test.SyntaxProcessor/AddExecuteLoggerVisitorTest.cs
+++ Katarina/Test.SyntaxProcessor/AddExecuteLoggerVisitorTest.cs
@@ -20,24 +20,28 @@ namespace Test.SyntaxProcessor
         public void VariableDeclaratorTest()
         {
             var source = "var a = 0";
-            var fullSource = "class C { void M() { " + source + "; } }";
-            var syntaxTree = CSharpSyntaxTree.ParseText(fullSource);
-            var compilationUnit = (CompilationUnitSyntax)syntaxTree.GetRoot();
-            var typeDeclaration = (TypeDeclarationSyntax)compilationUnit.Members[0];
-            var methodDeclaration = (MethodDeclarationSyntax)typeDeclaration.Members[0];
-            var methodBody = methodDeclaration.Body;
-            var firstStatement = methodBody.Statements[0];
+            var tree = ParseMethodBody(source);
 
             var visitor = new AddExecuteLoggerVisitor();
-            var newFirstStatement = visitor.Visit(firstStatement);
+            var newTree = visitor.Visit(tree);
 
-            Assert.AreEqual(@"var a = AV.Cyclone.Katrina.Executor.Context.ExecuteLogger.LogAssign(""a"","""",0,0);", newFirstStatement.ToString());
+            Assert.AreEqual(@"var a = AV.Cyclone.Katrina.Executor.Context.ExecuteLogger.LogAssign(""a"","""",0,0);", newTree.ToString());
         }
 
         [Test]
         public void VariableAssignTest()
         {
             var source = "a = 0";
+            var tree = ParseMethodBody(source);
+
+            var visitor = new AddExecuteLoggerVisitor();
+            var newTree = visitor.Visit(tree);
+
+            Assert.AreEqual(@"a = AV.Cyclone.Katrina.Executor.Context.ExecuteLogger.LogAssign(""a"","""",0,0);", newTree.ToString());
+        }
+
+        private static StatementSyntax ParseMethodBody(string source)
+        {
             var fullSource = "class C { void M() { " + source + "; } }";
             var syntaxTree = CSharpSyntaxTree.ParseText(fullSource);
             var compilationUnit = (CompilationUnitSyntax)syntaxTree.GetRoot();
@@ -45,11 +49,7 @@ namespace Test.SyntaxProcessor
             var methodDeclaration = (MethodDeclarationSyntax)typeDeclaration.Members[0];
             var methodBody = methodDeclaration.Body;
             var firstStatement = methodBody.Statements[0];
-
-            var visitor = new AddExecuteLoggerVisitor();
-            var newFirstStatement = visitor.Visit(firstStatement);
-
-            Assert.AreEqual(@"a = AV.Cyclone.Katrina.Executor.Context.ExecuteLogger.LogAssign(""a"","""",0,0);", newFirstStatement.ToString());
+            return firstStatement;
         }
     }
 }
