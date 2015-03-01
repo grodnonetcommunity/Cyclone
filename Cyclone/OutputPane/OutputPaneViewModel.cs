bc36bc695a380f4e60c0dcc435c37f80f5a23595
max.v <maxvoitas@gmail.com>
2015-03-01 08:30:33 +0300

    add empty lines hide scrollbar update each time layout updates

--- Cyclone/OutputPane/OutputPaneViewModel.cs
+++ Cyclone/OutputPane/OutputPaneViewModel.cs
@@ -98,7 +98,7 @@ namespace AV.Cyclone.OutputPane
         {
             //TODO: added triggering of file update when document was changed
             if (ExamplesPackage.WeatherStation != null)
-                ExamplesPackage.WeatherStation.FileUpdated(ExamplesPackage.Dte.ActiveDocument.FullName, e.After.GetText());
+                ExamplesPackage.WeatherStation.FileUpdated(ExamplesPackage.Dte.ActiveDocument.FullName, e.Before.GetText());
 
             //IsInitMarginSet = new bool[Model.ViewObjectModel.Elements.Count];
             
@@ -111,9 +111,9 @@ namespace AV.Cyclone.OutputPane
 
         private void UpdateScroll(object sender, TextViewLayoutChangedEventArgs e)
         {
-            var args = e;
-            if (!args.VerticalTranslation)
-                return;
+//            var args = e;
+//            if (!args.VerticalTranslation)
+//                return;
 
             UpdateScrollInternal();
         }
