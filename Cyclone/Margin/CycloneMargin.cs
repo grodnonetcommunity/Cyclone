26d651ca9880dfb70475212860df70bd4a6e130d
werwolf.by <werwolf.by@gmail.com>
2015-03-08 21:08:11 +0300

    [*] Port new margin.

---
+++ Cyclone/Margin/CycloneMargin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Media;
+using AV.Cyclone.Sandy.Models;
+using Cyclon.Controls;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace AV.Cyclone.Margin
+{
+    class CycloneMargin : IWpfTextViewMargin
+    {
+        public const string MarginName = "Test.VisualStudioMargin";
+        private bool isDisposed;
+        private readonly MarginContent marginContent;
+
+        public CycloneMargin(IWpfTextView textView, ICloudCollection cloudCollection)
+        {
+            VSColorTheme.ThemeChanged += VsColorThemeOnThemeChanged;
+            this.marginContent = new MarginContent(new VisualStudioTextViewService(textView), cloudCollection);
+            VsColorThemeOnThemeChanged(new ThemeChangedEventArgs(0));
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(MarginName);
+        }
+
+        #region IWpfTextViewMargin Members
+
+        public System.Windows.FrameworkElement VisualElement
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return marginContent;
+            }
+        }
+
+        #endregion
+
+        #region ITextViewMargin Members
+
+        public double MarginSize
+        {
+            get
+            {
+                ThrowIfDisposed();
+                return this.marginContent.ActualWidth;
+            }
+        }
+
+        public bool Enabled
+        {
+            // The margin should always be enabled
+            get
+            {
+                ThrowIfDisposed();
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Returns an instance of the margin if this is the margin that has been requested.
+        /// </summary>
+        /// <param name="marginName">The name of the margin requested</param>
+        /// <returns>An instance of Test.VisualStudioMargin or null</returns>
+        public ITextViewMargin GetTextViewMargin(string marginName)
+        {
+            return (marginName == CycloneMargin.MarginName) ? (IWpfTextViewMargin)this : null;
+        }
+
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                GC.SuppressFinalize(this);
+                VSColorTheme.ThemeChanged -= VsColorThemeOnThemeChanged;
+                isDisposed = true;
+            }
+        }
+
+        #endregion
+
+        private void VsColorThemeOnThemeChanged(ThemeChangedEventArgs themeChangedEventArgs)
+        {
+            var editorColor = VSColorTheme.GetThemedColor(EnvironmentColors.EditorExpansionFillBrushKey);
+            this.marginContent.Background = new SolidColorBrush(editorColor.ToWpf());
+        }
+    }
+}
---
+++ Cyclone/VisualStudioTextViewService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Cyclon.Controls;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+
+namespace AV.Cyclone
+{
+    public class VisualStudioTextViewService : ITextViewService
+    {
+        private readonly IWpfTextView textView;
+
+        public event EventHandler LayoutChanged;
+
+        public VisualStudioTextViewService(IWpfTextView textView)
+        {
+            this.textView = textView;
+            this.textView.LayoutChanged += (sender, args) => OnLayoutChanged();
+            this.textView.ZoomLevelChanged += (sender, args) => OnLayoutChanged();
+        }
+
+        public double Scale
+        {
+            get { return textView.ZoomLevel / 100; }
+        }
+
+        public int FirstVisibleLineNumber
+        {
+            get { return GetLineNumber(textView.TextViewLines.FirstVisibleLine); }
+        }
+
+        public int LastVisibleLineNumber
+        {
+            get { return GetLineNumber(textView.TextViewLines.LastVisibleLine); }
+        }
+
+        public IEnumerable<LineInfo> GetVisibleLines()
+        {
+            var firstVisibleLineNumber = FirstVisibleLineNumber;
+            var lastVisibleLineNumber = LastVisibleLineNumber;
+            foreach (var viewLine in textView.TextViewLines)
+            {
+                var lineNumber = GetLineNumber(viewLine);
+                if (lineNumber < firstVisibleLineNumber ||
+                    lineNumber > lastVisibleLineNumber)
+                    continue;
+                yield return new LineInfo
+                {
+                    LineNumber = lineNumber,
+                    TextTop = viewLine.TextTop - textView.ViewportTop
+                };
+            }
+        }
+
+        private static int GetLineNumber(ITextViewLine line)
+        {
+            return line.Snapshot.GetLineNumberFromPosition(line.Start);
+        }
+
+        protected virtual void OnLayoutChanged()
+        {
+            var handler = LayoutChanged;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+    }
+}
