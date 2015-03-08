0a27d9e15c7c8a6b462bb49ad9bf02c0536cd6c3
werwolf.by <werwolf.by@gmail.com>
2015-03-08 20:54:19 +0300

    [+] Port *EmptyLineTransformSource* and *EmptyLineTransformSourceProvider* mock.

---
+++ Cyclone/EmptyLines/EmptyLineTransformSource.cs
@@ -0,0 +1,33 @@
+﻿using AV.Cyclone.Sandy.Models;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+
+namespace AV.Cyclone.EmptyLines
+{
+    public class EmptyLineTransformSource : ILineTransformSource
+    {
+        private readonly ICloudCollection cloudCollection;
+
+        public EmptyLineTransformSource(ICloudCollection cloudCollection)
+        {
+            this.cloudCollection = cloudCollection;
+        }
+
+        public LineTransform GetLineTransform(ITextViewLine line, double yPosition, ViewRelativePosition placement)
+        {
+            if (cloudCollection == null)
+                return line.LineTransform;
+
+            var lineNumber = line.Snapshot.GetLineNumberFromPosition(line.Start.Position);
+            var height = cloudCollection.GetHeight(lineNumber);
+
+            if (line.Height < height)
+            {
+                return new LineTransform(line.LineTransform.TopSpace,
+                    line.LineTransform.BottomSpace + (height - line.Height), line.LineTransform.VerticalScale);
+            }
+
+            return line.LineTransform;
+        }
+    }
+}
---
+++ Cyclone/EmptyLines/EmptyLineTransformSourceProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Composition;
+using AV.Cyclone.Service;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.Utilities;
+
+namespace AV.Cyclone.EmptyLines
+{
+    [Export(typeof(ILineTransformSourceProvider))]
+    [ContentType("CSharp")]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    public class EmptyLineTransformSourceProvider : ILineTransformSourceProvider
+    {
+        [Import]
+        public ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        [Import]
+        public ICycloneService CycloneService { get; set; }
+
+        public ILineTransformSource Create(IWpfTextView textView)
+        {
+            /*var cloudCollection = CycloneService.GetClouds(textView);
+            if (cloudCollection == null)
+                return null;
+
+            return new EmptyLineTransformSource(cloudCollection);*/
+            return null;
+        }
+    }
+}
