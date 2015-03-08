cb5fd0965aae4a746317d976c9eeaa205761a005
werwolf.by <werwolf.by@gmail.com>
2015-03-08 20:59:25 +0300

    [+] Add [Cyclon.Controls] project.

---
+++ Cyclone.Controls/ITextViewService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cyclon.Controls
+{
+    public interface ITextViewService
+    {
+        event EventHandler LayoutChanged;
+
+        double Scale { get; }
+
+        int FirstVisibleLineNumber { get; }
+
+        int LastVisibleLineNumber { get; }
+
+        IEnumerable<LineInfo> GetVisibleLines();
+    }
+}
---
+++ Cyclone.Controls/MarginContent.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Controls;
+using AV.Cyclone.Sandy.Models;
+
+namespace Cyclon.Controls
+{
+    public partial class MarginContent
+    {
+        private readonly ITextViewService textViewService;
+        private readonly ICloudCollection cycloneService;
+
+        public MarginContent(ITextViewService textViewService, ICloudCollection cycloneService)
+        {
+            this.textViewService = textViewService;
+            this.cycloneService = cycloneService;
+            InitializeComponent();
+            this.DataContext = this;
+            this.textViewService.LayoutChanged += TextViewOnLayoutChanged;
+            UpdateScroll();
+        }
+
+        private void UpdateScroll()
+        {
+            var scale = textViewService.Scale;
+            contentCanvas.Children.Clear();
+            foreach (var line in textViewService.GetVisibleLines())
+            {
+                var lineControl = cycloneService.GetCloud(line.LineNumber);
+                if (lineControl == null)
+                    continue;
+
+                if (!ReferenceEquals(lineControl.Parent, this) && lineControl.Parent != null)
+                    lineControl.Parent.RemoveChild(lineControl);
+
+                if (lineControl is Control)
+                {
+                    ((Control)lineControl).FontSize = 12 * scale;
+                }
+
+                contentCanvas.Children.Add(lineControl);
+                Canvas.SetLeft(lineControl, 0);
+                Canvas.SetTop(lineControl, line.TextTop * scale);
+            }
+        }
+
+        private void TextViewOnLayoutChanged(object sender, EventArgs eventArgs)
+        {
+            UpdateScroll();
+        }
+    }
+}
